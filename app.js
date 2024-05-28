851b6b345f5d415e808ab11c8bf34dea33a31c5b
Angel Ojeda <angel.ojeda.castro@gmail.com>
2024-05-28 16:41:04 -0700

    added cors library

--- app.js
+++ app.js
@@ -3,18 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
