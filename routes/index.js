e614c77f890ea684c9c803b17141885cd1b954d5
Angel Ojeda <angel.ojeda.castro@gmail.com>
2024-05-28 23:33:47 -0700

    nueva ruta

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/saludo', function(req, res, next) {
+  res.json({saludo:"Hola Mundo"});
+});
 
 module.exports = router;
